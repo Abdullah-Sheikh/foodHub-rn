2e07f8ab29ebdb3f15353fcce22f2ec0545e3d3a
Abdullah Shaikh <abdullahshaikh@tk-lpt-0831.tkxel.com>
2023-01-20 16:31:30 +0500

    Add text Styles in project

--- app/utils/textStyles.js
+++ app/utils/textStyles.js
@@ -1,9 +1,49 @@
 import { StyleSheet } from "react-native/types";
 
 
-const textStyles = {
+const textStyles =  StyleSheet.create({
 
     h1:{
-        
-    }
-}
+        fontSize:36,
+        lineHeight:45,
+        color:'#000000',
+        fontFamily:'SofiaPro-Bold',
+    },
+    h2:{
+        fontSize:30,
+        lineHeight:40,
+        color:'#000000',
+        fontFamily:'SofiaPro-Medium',
+    },
+    h3:{
+        fontSize:24,
+        color:'#000000',
+        fontFamily:'SofiaPro-Medium',
+    },
+    h4:{
+        fontSize:18,
+        color:'#000000',
+        fontFamily:'SofiaPro-Bold',
+    },
+    h5:{
+        fontSize:16,
+        lineHeight:22,
+        color:'#000000',
+        fontFamily:'SofiaPro-Medium',
+    },
+    h6:{
+        fontSize:15,
+        lineHeight:21,
+        color:'#000000',
+        fontFamily:'SofiaPro-Regular',
+    },
+    h:{
+        fontSize:12,
+        color:'#000000',
+        fontFamily:'SofiaPro-Regular',
+    },
+
+
+
+
+});
