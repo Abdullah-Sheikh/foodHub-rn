150c0f27e1bb4645b7284e8ec2cca91b5acc9ca0
Abdullah-sheikh <abdullahsh123@hotmail.com>
2023-02-01 18:31:11 +0500

    Address screen design completed

---
+++ app/components/AddAddress.js/AddressForm.js
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function AddressForm() {
+  return (
+    <View>
+     
+    </View>
+  )
+}
+
+const styles  =StyleSheet.create({
+    
+})
--- app/screens/AddressScreen.js
+++ app/screens/AddressScreen.js
@@ -1,13 +1,56 @@
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView , Alert} from 'react-native'
 import React from 'react'
 import Header from '../components/UI/Header';
+import Input from '../components/UI/Input';
+import PrimaryBtn from '../components/UI/PrimaryBtn';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default function AddressScreen() {
   return (
     <View style={styles.root}>
         <SafeAreaView>
+            
             <Header/>
-      
+            <ScrollView>
+
+            <Input 
+                label="Full Name" 
+                placeholder="your full name"
+                keyboardType="default"
+               />
+
+            <Input 
+                label="Mobile Number" 
+                placeholder="your mobile number"
+                keyboardType="default"
+               />
+
+            <Input 
+                label="State" 
+                placeholder="your current state"
+                keyboardType="default"
+               /> 
+
+            <Input 
+                label="City" 
+                placeholder="your current city"
+                keyboardType="default"
+               />  
+
+            <Input 
+                label="Street (Include house number)" 
+                placeholder="Street"
+                keyboardType="default"
+               />   
+
+
+               <PrimaryBtn
+               onPress={() => { Alert.alert("Coming soon")}}
+               label="SAVE"
+               />      
+
+            
+</ScrollView>
 
         </SafeAreaView>
      
@@ -20,7 +63,6 @@ const styles = StyleSheet.create({
     root:{
         backgroundColor:'#ffffff',
         flex:1,
-
     },
 
 
