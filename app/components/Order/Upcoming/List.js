72f8c27ade56379d96d3170bbabfadb3ecc12e6e
Abdullah-sheikh <abdullahsh123@hotmail.com>
2023-02-06 11:56:06 +0500

    Upcoming order List completed

--- app/components/Order/Upcoming/List.js
+++ app/components/Order/Upcoming/List.js
@@ -1,11 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 import Item from './Item'
 
 export default function List() {
+
+  const renderItem = ({item}) => {
+    return(
+      <Item/>
+    )
+  }
+
+
   return (
     <View>
-     <Item/>
+      <FlatList
+      data={[
+        {key:'1',}
+      ]}
+      renderItem={renderItem}
+      keyExtractor= {(item) => item.key}
+      />
+    
     </View>
   )
 }
