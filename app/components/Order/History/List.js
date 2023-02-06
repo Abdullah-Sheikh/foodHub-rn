e5a8cfcc13f83f3bfea7b6b662363eddc2a761bf
Abdullah-sheikh <abdullahsh123@hotmail.com>
2023-02-06 11:58:54 +0500

    previous  order List completed

--- app/components/Order/History/List.js
+++ app/components/Order/History/List.js
@@ -1,11 +1,26 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Item from './Item'
+import { FlatList } from 'react-native-gesture-handler'
 
 export default function List() {
+    const renderItem = () =>{
+        return(
+              <Item/>
+        )
+    }
   return (
     <View>
-     <Item/>
+
+        <FlatList
+        renderItem={renderItem}
+        data={[
+            {key:'1',},
+            {key:'2'}
+        ]}
+        keyExtractor={ (item) => item.key}
+        />
+     
     </View>
   )
 }
